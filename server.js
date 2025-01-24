47e4c6da64c4c205c38bd0f069b9cb1c59e98d58
unknown <rithanyasasikumar.18@mail.com>
2025-01-24 10:31:22 +0530

    done

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const express = express();
+const port = 3000;
+
+app.get('/route1', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
